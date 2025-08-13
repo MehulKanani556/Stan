770a7894fde9993046060eafe14d0ef432ec50a8
VaibhavD17 <vaibhav9723142179@gmail.com>
2025-08-13 17:59:56 +0530

    vaibhav13-08

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import Home from './components/Home';
 import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
+import Rewards from './components/Rewards';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/rewards" element={<Rewards />} />
       </Routes>
     </>
   );
