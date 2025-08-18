8811c33e54fb8f75cf645be200c0393ddccaa1a6
denish456 <denish.kalathiyainfotech@gmail.com>
2025-08-18 09:21:53 +0530

    remove-header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import GamePlay from './components/GamePlay';
 function App() {
   return (
     <>
-      {/* <Header /> */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
