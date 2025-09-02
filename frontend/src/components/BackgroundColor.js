27e0e96446dd3e5fdc8f1c1e467aa6c6e9ba36a0
mehulkanani556 <mkanani556@gmail.com>
2025-09-02 14:05:03 +0530

    implement cluster

--- frontend/src/components/BackgroundColor.js
+++ frontend/src/components/BackgroundColor.js
@@ -6,8 +6,8 @@ export default function BackgroundColor({ children, className = '' }) {
       {/* Background layer fixed to viewport */}
       <div className="fixed inset-0 -z-10 pointer-events-none overflow-hidden bg-gradient-to-b from-[#0f0d12] to-[#141216]">
         {/* Decorative blobs */}
-        <div className="pointer-events-none absolute -top-20 -right-24 w-[420px] h-[420px] rounded-full bg-[#902F7E]/30 blur-3xl" />
-        <div className="pointer-events-none absolute -bottom-24 -left-24 w-[420px] h-[420px] rounded-full bg-[#902F7E]/30 blur-3xl" />
+        <div className="pointer-events-none absolute -top-20 -right-24 w-[420px] h-[420px] rounded-full bg-[#482679]/30 blur-3xl" />
+        <div className="pointer-events-none absolute -bottom-24 -left-24 w-[420px] h-[420px] rounded-full bg-[#972F7C]/30 blur-3xl" />
         <div className="pointer-events-none absolute -bottom-40 -right-40 w-[320px] h-[320px] rounded-full bg-[#2F7E90]/20 blur-2xl" />
       </div>
       {children}
