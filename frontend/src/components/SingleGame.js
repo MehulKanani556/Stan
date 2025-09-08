1d590962c9e1a5c045fa6c0dd2fc5bb9a45a3899
mehulkanani556 <mkanani556@gmail.com>
2025-09-08 09:59:29 +0530

    footer

--- frontend/src/components/SingleGame.js
+++ frontend/src/components/SingleGame.js
@@ -243,21 +243,21 @@ const SingleGame = () => {
     setShowPaymentForm(true);
   };
 
-  const formatDate = (dateString) => {
+  // const formatDate = (dateString) => {
 
-    if (!dateString) return "";
+  //   if (!dateString) return "";
   
-    const date = new Date(dateString);
+  //   const date = new Date(dateString);
   
-    const day = String(date.getDate()).padStart(2, "0");
+  //   const day = String(date.getDate()).padStart(2, "0");
   
-    const month = String(date.getMonth() + 1).padStart(2, "0");
+  //   const month = String(date.getMonth() + 1).padStart(2, "0");
   
-    const year = String(date.getFullYear()).slice(-2);
+  //   const year = String(date.getFullYear()).slice(-2);
   
-    return `${day}-${month}-${year}`;
+  //   return `${day}-${month}-${year}`;
   
-  };
+  // };
    
 
   const handlePaymentSuccess = () => {
